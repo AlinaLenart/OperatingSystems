ff961e15cc8268b2a12648faf30a7b02762fd3f6
Alina Lenart <alinalenart258@gmail.com>
2024-05-14 11:54:20 +0200

    OPT, LRU, RAND

--- Symulacja3/src/Algorithms.java
+++ Symulacja3/src/Algorithms.java
@@ -1,19 +1,16 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Algorithms {
-    private List<Integer> pageRefenerences;
+    private List<Integer> pageReferences;
     private int ramSize;
 
-    public Algorithms(List<Integer> pageRefenerences, int ramSize) {
-        this.pageRefenerences = pageRefenerences;
+    public Algorithms(List<Integer> pageReferences, int ramSize) {
+        this.pageReferences = pageReferences;
         this.ramSize = ramSize;
     }
 
     public void FIFO(){
-        Queue<Integer> pages = new LinkedList<>(pageRefenerences);
+        Queue<Integer> pages = new LinkedList<>(pageReferences);
         int[] ram = new int[ramSize];
         int pageFaults = 0;
 
@@ -33,8 +30,88 @@ public class Algorithms {
         }
 
         System.out.println("Liczba błędów strony (FIFO): " + pageFaults);
-
     }
+
+    public void RAND() {
+        int[] ram = new int[ramSize];
+        int pageFaults = 0;
+        Random generate = new Random();
+        Queue<Integer> pages = new LinkedList<>(pageReferences);
+
+        while (!pages.isEmpty()) {
+            int page = pages.poll();
+            if (!hasElement(ram, page)) { // If the page is not in RAM
+                if (isFull(ram)) { // If RAM is full
+                    // Replace a random page in RAM
+                    int randomIndex = generate.nextInt(ramSize);
+                    ram[randomIndex] = page;
+                    pageFaults++;
+                } else {
+                    addPageToRAM(ram, page);
+                    pageFaults++;
+                }
+            }
+            displayRam(ram);
+        }
+
+        System.out.println("Liczba błędów strony (RAND): " + pageFaults);
+    }
+
+    public void OPT() {
+        int[] ram = new int[ramSize];
+        int pageFaults = 0;
+        Queue<Integer> pages = new LinkedList<>(pageReferences);
+
+        while (!pages.isEmpty()) {
+
+            int page = pages.poll();
+
+            if (!hasElement(ram, page)) { // If the page is not in RAM
+
+                if (isFull(ram)) { // If RAM is full
+                    int replaceIndex = getOptimalReplacement(ram, pages);
+                    ram[replaceIndex] = page; // Replace the page at the optimal index
+                    pageFaults++;
+                } else {
+                    addPageToRAM(ram, page); // Add the page to RAM
+                    pageFaults++;
+                }
+            }
+            displayRam(ram);
+        }
+
+        System.out.println("Liczba błędów strony (OPT): " + pageFaults);
+    }
+
+    public void LRU() {
+        Queue<Integer> pages = new LinkedList<>(pageReferences);
+        int[] ram = new int[ramSize];
+        int[] usageHistory = new int[ramSize]; // Array to track usage history
+        int pageFaults = 0;
+
+        while (!pages.isEmpty()) {
+            int page = pages.poll();
+            if (!hasElement(ram, page)) { // If the page is not in RAM
+                if (isFull(ram)) { // If RAM is full
+                    int replaceIndex = getLeastRecentlyUsedIndex(usageHistory); // Find the index of the least recently used page
+                    ram[replaceIndex] = page; // Replace the least recently used page
+                    updateUsageHistory(usageHistory, replaceIndex); // Update usage history
+                    pageFaults++;
+                } else {
+                    addPageToRAM(ram, page); // Add the page to RAM
+                    updateUsageHistory(usageHistory, ramSize - 1); // Update usage history
+                    pageFaults++;
+                }
+            } else { // If the page is already in RAM, update its usage history
+                int index = getIndexInRam(ram, page);
+                updateUsageHistory(usageHistory, index); // Update usage history
+            }
+            displayRam(ram);
+        }
+
+        System.out.println("Liczba błędów strony (LRU): " + pageFaults);
+    }
+
 
     private boolean isFull(int[] array) {
         boolean isFull = true;
@@ -71,6 +148,64 @@ public class Algorithms {
         }
         System.out.println();
     }
+
+    private int getOptimalReplacement(int[] ram, Queue<Integer> remainingPages) {
+        int maxIndex = -1;
+        int replacePageIndex = -1;
+
+        for (int i = 0; i < ram.length; i++) {
+            int pageIndex = getIndex(ram[i], remainingPages); // Get the index of the current page in the remaining pages
+            if (pageIndex == -1) { // If the page won't be used anymore
+                return i; // Return the index of this page for replacement in the RAM array
+            }
+            if (pageIndex > maxIndex) {
+                maxIndex = pageIndex;
+                replacePageIndex = i;
+            }
+        }
+
+        return replacePageIndex;
+    }
+
+    private int getIndex(int page, Queue<Integer> remainingPages) {
+        int index = 0;
+        for (int p : remainingPages) {
+            if (p == page) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+    private int getIndexInRam(int[] array, int element) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void updateUsageHistory(int[] usageHistory, int index) {
+        for (int i = 0; i < usageHistory.length; i++) {
+            if (i == index) {
+                usageHistory[i] = 0; // Reset usage history for the recently used page
+            } else {
+                usageHistory[i]++; // Increment usage history for other pages
+            }
+        }
+    }
+
+    private int getLeastRecentlyUsedIndex(int[] usageHistory) {
+        int maxIndex = 0;
+        for (int i = 1; i < usageHistory.length; i++) {
+            if (usageHistory[i] > usageHistory[maxIndex]) { //im wiecej jednostek temu zostal uzyty tym mam wiecej w usageHistory
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+
 
 
 }
--- Symulacja3/src/Main.java
+++ Symulacja3/src/Main.java
@@ -2,16 +2,19 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int totalReferences = 50;
+        int totalReferences = 20;
         List<Integer> pageReferences = Generator.generatePageReferences(totalReferences);
         generatedPrint(totalReferences, pageReferences);
 
         int ramSize = 5;
         Algorithms alg = new Algorithms(pageReferences, ramSize);
         alg.FIFO();
-
-
-
+        System.out.println();
+        alg.RAND();
+        System.out.println();
+        alg.OPT();
+        System.out.println();
+        alg.LRU();
 
 
 
